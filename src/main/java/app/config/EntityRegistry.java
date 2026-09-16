659fb5e7cd1edd9eb6967d93193a7c7e8efdd43f
MathiasStenvall <cph-ms1422@stud.ek.dk>
2026-09-16 11:06:43 +0200

    - added genre to entityRegistry

--- src/main/java/app/config/EntityRegistry.java
+++ src/main/java/app/config/EntityRegistry.java
@@ -2,6 +2,7 @@ package app.config;
 
 import app.entities.Actor;
 import app.entities.Crew;
+import app.entities.Genre;
 import app.entities.Movie;
 import org.hibernate.cfg.Configuration;
 
@@ -15,5 +16,6 @@ final class EntityRegistry {
         configuration.addAnnotatedClass(Movie.class);
         configuration.addAnnotatedClass(Crew.class);
         configuration.addAnnotatedClass(Actor.class);
+        configuration.addAnnotatedClass(Genre.class);
     }
 }
